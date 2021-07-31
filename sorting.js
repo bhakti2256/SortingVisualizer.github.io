18cbd8e7b3210439466e58b43d2c7ee141373063
bhakti2256 <77962432+bhakti2256@users.noreply.github.com>
2021-07-31 15:24:48 +0530

    Update

--- sorting.js
+++ sorting.js
@@ -81,9 +81,11 @@ function disableButton(buttonId, status) {
             if (status) {
                 button[i].disabled = true;
                 document.getElementById("myRange").disabled = true;
+                document.getElementById("speedSlider").disabled = true;
             } else {
                 button[i].disabled = false;
                 document.getElementById("myRange").disabled = false;
+                document.getElementById("speedSlider").disabled = false;
             }
         }
     }
